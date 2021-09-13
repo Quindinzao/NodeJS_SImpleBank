bde4e52c54f4776f1b93f022414ded2e839800ac
João Victor Fernandes <fernandes@softable.com.br>
2021-09-13 20:53:11 -0300

    feat: implementa novas funcionalidades

--- src/index.js
+++ src/index.js
@@ -104,4 +104,50 @@ app.post("/withdraw", verifyIsExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/date", verifyIsExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIsExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIsExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIsExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  // splice
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+// app.get("/balance", verifyIsExistsAccountCPF, (request, response) => {
+//   const { customer } = request;
+
+//   const balance = getBalance(customer.statement);
+
+//   return response.json(balance);
+// });
+
 app.listen(3333);
